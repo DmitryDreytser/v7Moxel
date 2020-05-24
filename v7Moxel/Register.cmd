175ed9b358c0006fec75c236bfdf8f3a838ab7c2
Дмитрий Дрейцер <maddad@mail.ru>
2020-05-24 22:50:44 +0300

    Добавлен скрипт регистрации

---
+++ v7Moxel/Register.cmd
@@ -0,0 +1,4 @@
+%~D0
+CD "%~DP0"
+%systemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe "%~DP0v7Moxel.dll" /unregister
+%systemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe "%~DP0v7Moxel.dll" /codebase
