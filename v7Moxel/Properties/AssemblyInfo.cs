cc551480cd21c280a32ad35db41e260ec042de9e
maddad <maddad@mail.ru>
2024-06-18 17:11:50 +0300

    Небольшая правка форматирования ячеек выровненных по правому краю.

--- v7Moxel/Properties/AssemblyInfo.cs
+++ v7Moxel/Properties/AssemblyInfo.cs
@@ -11,14 +11,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Licence Owner")]
 [assembly: AssemblyProduct("v7Moxel")]
-[assembly: AssemblyCopyright("Copyright © Licence Owner 2019")]
+[assembly: AssemblyCopyright("Copyright © MadDAD 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
 // для компонентов COM. Если необходимо обратиться к типу в этой сборке через
 // COM, задайте атрибуту ComVisible значение TRUE для этого типа.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("c896056b-d75b-49e0-9d56-871b75f5de51")]
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.9")]
-[assembly: AssemblyFileVersion("0.0.0.9")]
+[assembly: AssemblyVersion("0.0.0.10")]
+[assembly: AssemblyFileVersion("0.0.0.10")]
 [assembly: NeutralResourcesLanguage("ru")]
 
