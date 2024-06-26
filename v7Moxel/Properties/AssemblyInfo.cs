e37ead069edf45b5ee82b0fc7ec92bf01ef40263
Дмитрий Дрейцер <maddad@mail.ru>
2024-06-26 03:13:47 +0300

    version bump

--- v7Moxel/Properties/AssemblyInfo.cs
+++ v7Moxel/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.10")]
-[assembly: AssemblyFileVersion("0.0.0.10")]
+[assembly: AssemblyVersion("0.0.0.11")]
+[assembly: AssemblyFileVersion("0.0.0.11")]
 [assembly: NeutralResourcesLanguage("ru")]
 
