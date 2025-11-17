d4f4c1dd5fe2cc7eb18069543823362e61111896
maddad <maddad@mail.ru>
2025-11-17 11:11:20 +0300

    version bump

--- v7Moxel/Register.cmd
+++ v7Moxel/Register.cmd
@@ -1,4 +1,3 @@
 %~D0
 CD "%~DP0"
-%systemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe "%~DP0v7Moxel.dll" /unregister
 %systemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe "%~DP0v7Moxel.dll" /codebase
