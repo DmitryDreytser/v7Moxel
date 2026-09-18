2f8a0367344f7338842e2f7c45e38c614484e2a9
Дмитрий Дрейцер <maddad@mail.ru>
2026-09-19 00:14:56 +0300

    метаданные

--- v7Moxel/Properties/AssemblyInfo.cs
+++ v7Moxel/Properties/AssemblyInfo.cs
@@ -7,18 +7,18 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("v7Moxel")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Внешняя компонентf 1C 7.7 для конвертации mxl в Excel-2007, HTML, PDF")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MadDAD")]
+[assembly: AssemblyCompany("Дмитрий Дрейцер")]
 [assembly: AssemblyProduct("v7Moxel")]
-[assembly: AssemblyCopyright("Copyright © MadDAD 2024")]
+[assembly: AssemblyCopyright("Copyright © Дмитрий Дрейцер 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
 // для компонентов COM. Если необходимо обратиться к типу в этой сборке через
 // COM, задайте атрибуту ComVisible значение TRUE для этого типа.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("c896056b-d75b-49e0-9d56-871b75f5de51")]
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.11")]
-[assembly: AssemblyFileVersion("0.0.0.11")]
+[assembly: AssemblyVersion("0.0.0.12")]
+[assembly: AssemblyFileVersion("0.0.0.12")]
 [assembly: NeutralResourcesLanguage("ru")]
 
